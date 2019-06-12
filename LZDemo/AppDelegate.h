5b4166be23ffd212cb7e3590bd1308ed5b597c4e
fankai <fank@izkml.com>
2019-06-12 09:24:06 +0800

    Initial Commit

---
+++ LZDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LZDemo
+//
+//  Created by fank on 2019/6/12.
+//  Copyright © 2019 fank. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LZDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LZDemo
+//
+//  Created by fank on 2019/6/12.
+//  Copyright © 2019 fank. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
